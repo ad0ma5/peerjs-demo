825bd545e7a98741b352da175248d3042d0a4880
Adomas <adomas@b5277.k.dedikuoti.lt>
2021-10-27 04:07:05 +0300

    data

--- server/accounts.js
+++ server/accounts.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
 
 
 function accounts(req, res, next)  {
 	console.log('accounts');
-	res.send('Hello accounts world!')
 
+	const content = 'Some content!'
+
+	fs.writeFile('./data/test.txt', content, err => {
+		  if (err) {
+				 console.error(err)
+	       res.send('Hello accounts world! file write ok')
+				    return
+			}
+		fs.readFile('./data/test.txt', 'utf8' , (err, data) => {
+			  if (err) {
+					    console.error(err)
+					    return
+					  }
+			  console.log(data)
+	    res.send('Hello accounts world! file write ok data='+data)
+		})
+		  //file written successfully
+	})
 }
 module.exports = accounts;
